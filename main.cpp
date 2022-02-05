ea2d54dedac6b1d728545e8f56e127ff1026c956
Irina Yarysheva <iyarysheva@gmail.com>
2022-02-05 19:26:28 +0300

    added timer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <chrono>
+#include <cmath>
+
+using namespace std::chrono;
+
+class Timer{
+public:
+    Timer(): start(steady_clock::now()) {}
+    ~Timer() {
+        std::cout << "Duration is " <<
+                  duration_cast<microseconds>(steady_clock::now() - start).count()
+                  << std::endl;
+    }
+
+private:
+    steady_clock::time_point start;
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto result = 0.;
+    {
+        Timer t;
+        for(auto i = 0u; i < 1'000'000u; i++) {
+            result += sin(i) * cos(i);
+        }
+    }
+    std::cout << "Result is " << result << std::endl;
+    std::cout << "Hello, DPQE, DMCP!" << std::endl;
     return 0;
 }
